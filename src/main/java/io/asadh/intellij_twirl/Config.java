71baf3fc2d92bc83e5e384aa4361abe8998b853b
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-06-20 02:26:16 +0100

    feat: java rewrites

---
+++ src/main/java/io/asadh/intellij_twirl/Config.java
@@ -0,0 +1,5 @@
+package io.asadh.intellij_twirl;
+
+public class Config {
+    public static String ICON_PATH = "/icons/play.svg";
+}
---
+++ src/main/java/io/asadh/intellij_twirl/language/TwirlIcons.java
@@ -0,0 +1,11 @@
+package io.asadh.intellij_twirl.language;
+
+import io.asadh.intellij_twirl.Config;
+
+import javax.swing.Icon;
+
+import com.intellij.openapi.util.IconLoader;
+
+public class TwirlIcons {
+  public static final Icon FILE = IconLoader.getIcon(Config.ICON_PATH, TwirlIcons.class);
+}
